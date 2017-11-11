6b895c9b522520262a389b90373f246f9c24db3d
Oleg <oleg.pav.m@gmail.com>
2017-11-11 11:34:55 +0300

    Polo stub.

--- Polo.rb
+++ Polo.rb
@@ -1,6 +1,12 @@
 require 'http'
+require 'openssl'
+require 'json'
 
 class Polo
+
+	def initialize
+		@key, @secret = File.read('keys.txt').split("\n")
+	end
 
 	def have(pair)
 		#
@@ -8,6 +14,20 @@ class Polo
 
 	def sell(pair)
 		#
+	end
+
+	private
+
+	def api_call
+		#
+	end
+
+	def headers
+		#
+	end
+
+	def readable(num)
+		'%1.8f' % num.to_f
 	end
 
 end
